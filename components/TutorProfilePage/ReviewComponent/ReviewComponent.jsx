263ff74737236f0417c8ee7d87200f3f17cec90e
Raj <rajkushwah9968117019@gmail.com>
2022-07-20 22:23:09 +0530

    added reviews in the db

--- components/TutorProfilePage/ReviewComponent/ReviewComponent.jsx
+++ components/TutorProfilePage/ReviewComponent/ReviewComponent.jsx
@@ -7,7 +7,7 @@ const ReviewComponent = ({ tutor }) => {
   return (
     <div className="mt-6">
       <ScrollArea style={{ height: 350 }}>
-        {user.notReviewed.includes(uid) && <ReviewForm />}
+        {user.notReviewed.includes(uid) && <ReviewForm tutor={tutor} />}
       </ScrollArea>
     </div>
   );
--- components/TutorProfilePage/ReviewForm/ReviewForm.jsx
+++ components/TutorProfilePage/ReviewForm/ReviewForm.jsx
@@ -1,11 +1,49 @@
 import { Textarea, Text, Button } from "@mantine/core";
+import { showNotification } from "@mantine/notifications";
 import { useState } from "react";
 import { FaStar } from "react-icons/fa";
-const ReviewForm = () => {
+import { collection, addDoc } from "firebase/firestore";
+import { useLocalStorage } from "@mantine/hooks";
+import { db } from "../../../firebase/firebaseConfig";
+const ReviewForm = ({ tutor }) => {
+  const { uid: tutorId } = tutor;
   const [rating, setRating] = useState(0);
   const [hover, setHover] = useState(0);
+  const [body, setBody] = useState("");
+  const [bodyError, setBodyError] = useState("");
+
+  const [user] = useLocalStorage({ key: "user-data" });
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    if (rating < 1) {
+      showNotification({
+        autoClose: 3000,
+        title: "Please Rate the tutor",
+        message: "Rating can't be empty",
+        color: "red",
+      });
+      return;
+    }
+    if (body.trim().length < 6) {
+      setBodyError("Please write a valid review");
+      return;
+    }
+
+    const reviewRef = await addDoc(collection(db, "reviews"), {
+      body,
+      rating,
+      from: user.uid,
+      of: tutorId,
+    });
+    console.log(reviewRef);
+  };
+
   return (
-    <form className="border rounded-lg p-2 sm:p-4 dark:border-dark-400">
+    <form
+      onSubmit={submitHandler}
+      className="border rounded-lg p-2 sm:p-4 dark:border-dark-400"
+    >
       <div className="mb-4">
         <Text className="text-xl" color="dimmed">
           Leave a review
@@ -49,9 +87,17 @@ const ReviewForm = () => {
         minRows={3}
         label="Write a review"
         required
+        value={body}
+        onChange={(e) => {
+          setBody(e.target.value);
+          setBodyError("");
+        }}
+        error={bodyError}
       />
       <div className="mt-4 flex justify-end ">
-        <Button className="button-primary">Submit</Button>
+        <Button type="submit" className="button-primary">
+          Submit
+        </Button>
       </div>
     </form>
   );
